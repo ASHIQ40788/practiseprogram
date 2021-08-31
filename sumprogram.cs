bfd3bb56d3bc51dd89cad25db87e42bdfed90f8e
ShaikAshiq <ashiqshaiksvali8@gmail.comgit config --global user.email ashiqshaiksvali8@gmail.com>
2021-08-31 16:50:54 +0530

    first commit

---
+++ sumprogram.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyApplication
+{
+    class sumprogram
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 6;
+            int sum = x + y;
+            Console.WriteLine(sum); // Print the sum of x + y
+        }
+    }
+}
