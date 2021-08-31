f18ba212482807832e4943e32b657b3b6b9054e3
ShaikAshiq <ashiqshaiksvali8@gmail.comgit config --global user.email ashiqshaiksvali8@gmail.com>
2021-08-31 19:44:25 +0530

    once again changed values of X and Y

--- sumprogram.cs
+++ sumprogram.cs
@@ -7,8 +7,8 @@ namespace MyApplication
         static void Main(string[] args)
         {
             //changes for HIS-122..
-            int x = 40788;
-            int y = 788;
+            int x = 150040788;
+            int y = 150040788;
             int sum = x + y;
             Console.WriteLine(sum); // Print the sum of x + y
         }
