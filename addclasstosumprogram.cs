aa4506ace98149fa9e71a4212b92d59e78ed44da
ShaikAshiq <ashiqshaiksvali8@gmail.comgit config --global user.email ashiqshaiksvali8@gmail.com>
2021-08-31 20:08:36 +0530

    HIS-122 COMPLETED

--- addclasstosumprogram.cs
+++ addclasstosumprogram.cs
@@ -8,5 +8,12 @@ namespace practisecode
 {
     class Class1
     {
+        public void start()
+        {
+            //changes for HIS-122
+            int i = 16;
+            int i = 18;
+            int i = 22;
+        }
     }
 }
